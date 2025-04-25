f388d73906ee9e1604f0eb9f7031e6c4abe509db
AngelusPY <angeloadinolffi83@gmail.com>
2025-04-25 00:03:20 -0300

    lo ultimo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,11 +6,32 @@ const Display = ({counter}) => {
   )
 }
 
-const Button = ({onClick,text}) => {
+const handleClick = () => {
+  setAll(allClicks.concat('R'))
+  setRight(right + 1)
+  se
+  tTotal(left + right)
+}
+const Button = ({handleClick,text}) => {
   return(
-    <button onClick={onClick}>
+    <button onClick={handleClick}>
       {text}
     </button>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
@@ -18,15 +39,22 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    console.log('left before', left)
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    console.log('left after', updateLeft)
+    setTotal(updateLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    const updateRight = right + 1
+    setRight(updateRight)
+    setTotal(left + updateRight)
   }
 
   return (
@@ -35,7 +63,9 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <p>{allClicks.join(' - ')}</p>
+      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
