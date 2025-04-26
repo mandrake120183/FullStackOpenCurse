8de947b6de57ef70c5a4774e45e17d041b01a1d9
AngelusPY <angeloadinolffi83@gmail.com>
2025-04-26 15:02:26 -0300

    part1 anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,42 +1,74 @@
-import { useState } from "react"
+import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return(
-    <div>{counter}</div>
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    </div>
   )
 }
 
-const Button = ({onClick,text}) => {
-  return(
-    <button onClick={onClick}>
-      {text}
-    </button>
+const Display = (props) => {
+  return (
+    <div>
+      {props.anecdotes}
+      <p>has {props.vote} votes</p>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
   )
 }
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
+  
+  const handleNextClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+    console.log(randomIndex) 
   }
-  const decreasebyOne = () => {
-    console.log('decreasing, value before', counter) 
-    setCounter(counter - 1)
+
+  const handlerVoteClick = () => { 
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    
+    const mostVotes = Math.max(...copy) 
+    console.log('mostVotes',mostVotes) 
+    const mostVotesIndex = copy.indexOf(mostVotes)
+    console.log('mostVotesIndex',mostVotesIndex) 
+    setMostVotes(mostVotesIndex)
   }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+
+
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreasebyOne} text='minus'/>
+      <Header text={'Anecdote of the day'}/>
+      <Display anecdotes={anecdotes[selected]} vote={votes[selected]}/>
+      <Button onClick={handleNextClick} text={'next anectote'}/>
+      <Button onClick={handlerVoteClick} text={'vote'}/>
+      <Header text={'Anecdote with most votes'}/>
+      <Display anecdotes={anecdotes[mostVotes]} vote={votes[mostVotes]}/>
     </div>
   )
 }
