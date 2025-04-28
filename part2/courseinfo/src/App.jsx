075d3de33154e6cb08f10b07fc743b7e43ea73be
AngelusPY <angeloadinolffi83@gmail.com>
2025-04-27 22:15:10 -0300

    Componentes Controlados

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,53 +1,41 @@
 import { useState } from 'react'
-import Course from './components/Course'
+import Note from './components/Note'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes )
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
     }
-  ]
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
-  return <Course course={courses} />
+  const handleNotechange= (event) => {
+    console.log('event.target.value',event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNotechange} />
+        <button type="sudmit">save</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
